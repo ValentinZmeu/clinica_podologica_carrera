72c3b1e570936fc8334f924521a61be3fccb0017
vturcanu <vadim94t@gmail.com>
2025-12-29 20:02:18 +0100

    Project reset

--- public/assets/scripts.js
+++
@@ -1,3 +0,0 @@
-const btn = document.getElementById("menuBtn");
-const nav = document.getElementById("mobileNav");
-btn?.addEventListener("click", () => nav.classList.toggle("hidden"));
--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import "./bootstrap";
-
-const btn = document.getElementById("menuBtn");
-const nav = document.getElementById("mobileNav");
-btn?.addEventListener("click", () => nav.classList.toggle("hidden"));
